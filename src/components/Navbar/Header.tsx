f7543afe8c320ccf01d24df083587562239a037b
William Kohin <willim666.wl@gmailcom>
2022-07-03 23:33:07 +0700

    Fix unit menu

--- src/components/Navbar/Header.tsx
+++ src/components/Navbar/Header.tsx
@@ -1,10 +1,19 @@
 import { Popover } from "@headlessui/react";
-import { useStore } from "../../store/store";
+import { useNavigate } from "react-router-dom";
+import { IProfile, useStore } from "../../store/store";
 import { Link } from "../Links";
 
 export const Header = () => {
   const userProfile = useStore((state) => state.userProfile);
   console.log(userProfile);
+  const setUserProfile = useStore((state) => state.setUserProfile);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    console.log("hai");
+    setUserProfile({} as IProfile);
+    navigate("/");
+  };
+
   return (
     <div className="sticky top-0 flex justify-between px-8 py-4 text-white bg-red-600">
       <Link href="/">
@@ -23,6 +32,9 @@ export const Header = () => {
               <Link href="/admin/product">Traveler Panel</Link>
             )}
             {userProfile.role == 0 && <Link href="/shop">Traveler Page</Link>}
+            <span className="cursor-pointer" onClick={handleLogout}>
+              LogOut
+            </span>
           </>
         )}
       </div>
