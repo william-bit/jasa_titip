289115c5b815cb91578ab7ea4dff36ed1c11e051
William Kohin <willim666.wl@gmailcom>
2022-07-09 23:25:19 +0700

    fix profile admin role

--- src/pages/admin/Profile.tsx
+++ src/pages/admin/Profile.tsx
@@ -116,8 +116,8 @@ export const AdminProfile = () => {
         {userProfile.name}
       </h1>
       <h3 className="leading-6 text-gray-600 font-lg text-semibold">
-        Status as
-        {userProfile.role == 0 ? <span>Traveler</span> : <span>Customer</span>}
+        Status as{" "}
+        {userProfile.role == 1 ? <span>Traveler</span> : <span>Customer</span>}
       </h3>
       <p className="text-sm leading-6 text-gray-500 hover:text-gray-600"></p>
       <ul className="px-3 py-2 mt-3 text-gray-600 bg-gray-100 divide-y rounded shadow-sm hover:text-gray-700 hover:shadow">
